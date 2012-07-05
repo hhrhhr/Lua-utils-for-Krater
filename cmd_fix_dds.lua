543f259d8885feff0e3b33889c96bf85084cbc67
Dmitry V. Zaitsev <hhrhhr@gmail.com>
2012-07-06 02:29:08 +0400

    clean branch

--- cmd_fix_dds.lua
+++
@@ -1,8 +0,0 @@
-input_file = arg[1]
-assert(input_file, "no input file")
-
-require("util_binary_writer")
-local w = BinaryWriter
-w:update(input_file)
-w:int32(0)
-w:close()
--- scan_scripts_for_loc.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-set scripts=d:\tmp_Krater\scripts
-if exist work\strings_in_scripts.txt del /q work\strings_in_scripts.txt
-
-for /R %scripts% %%i in (*.lua) do (
-    echo %%i
-    lua parse_all_scripts_for_L.lua %%i >> work\strings_in_scripts.txt
-)
